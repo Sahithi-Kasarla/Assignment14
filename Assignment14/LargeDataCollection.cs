67e26e4bac2ce8ef2db38cb470e9fdf0e67d2657
Sahithi-kasarla <sahithikasarla123@gmail.com>
2023-11-17 14:10:16 +0530

    Assignment 14

---
+++ Assignment14/LargeDataCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14
+{
+    
+        public class LargeDataCollection<T> : IDisposable
+        {
+            private List<T> internalData; // List to hold data of any type
+
+            // Constructor to initialize the collection with initial data
+            public LargeDataCollection(IEnumerable<T> initialData)
+            {
+                internalData = new List<T>(initialData);
+            }
+
+            public void AddElement(T element)
+            {
+                internalData.Add(element);
+            }
+            public void RemoveElement(T element)
+            {
+                internalData.Remove(element);
+            }
+            public void AccessElements()
+            {
+                foreach (var element in internalData)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+
+            // Implementing the IDisposable interface
+            public void Dispose()
+            {
+                // Set the internal data structure to null to free up memory
+                internalData = null;
+                // Dispose method is called explicitly to release resources
+                GC.SuppressFinalize(this);
+            }
+        }
+    }
+
+
---
+++ Assignment14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             using (var intCollection = new LargeDataCollection<int>(new[] { 1, 2, 3, 4 }))
+            {
+                Console.WriteLine("Integer Collection:");
+                intCollection.AccessElements();
+
+                intCollection.AddElement(5);
+                intCollection.RemoveElement(2);
+                Console.WriteLine("\nCollection after adding One and removing One integer:");
+                intCollection.AccessElements();
+            }
+
+            using (var stringCollection = new LargeDataCollection<string>(new[] { "hari", "Indhu", "lalli" }))
+            {
+                Console.WriteLine("\nString Collection:");
+                stringCollection.AccessElements();
+
+                stringCollection.AddElement("Ram");
+                stringCollection.RemoveElement("Rani");
+                Console.WriteLine("\nCollection after adding One and removing One string:");
+                stringCollection.AccessElements();
+            }
+             Console.ReadKey();
+        }
+    }
+}
